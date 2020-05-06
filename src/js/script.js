0d5d1cb6b73807da469e0f3ecd9ec43e0cb5f986
Dawid Przydryga <shivva90@gmail.com>
2020-05-06 08:25:03 +0200

    Add personal-data page

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-unused-vars */
-function burgerSidebar(){
+function burgerSidebar() {
   const btn = document.getElementById('menu-toggle');
   const selectClass = { open: 'open', close: 'close' };
   const sidebar = document.getElementById('side');
   const body = document.getElementById('body');
   let btnClass = selectClass.open;
-  btn.addEventListener('click', function(){
+  btn.addEventListener('click', function () {
     if (btn.classList.contains(selectClass.open)) {
       btn.classList.remove(btnClass);
       btnClass = selectClass.close;
@@ -27,35 +27,35 @@ burgerSidebar();
 const quitIcon = document.getElementById('pop-quit');
 const profilIcon = document.getElementById('pop-profil');
 /* Pops-up */
-const  login = document.getElementById('login');
+const login = document.getElementById('login');
 const quit = document.getElementById('quit');
 /* Button */
 const btnCancel = document.getElementById('btnCancel');
 const btnQuit = document.getElementById('btnQuit');
-function popupsLogin(){
+function popupsLogin() {
   console.log(login);
   login.classList.add('up');
 }
-function popupsQuit(){
+function popupsQuit() {
   quit.classList.add('up');
 }
-function popupsCancel(){
+function popupsCancel() {
   login.classList.remove('up');
   quit.classList.remove('up');
 }
 
-function sidebarActive(){
+function sidebarActive() {
   const sidebars = document.querySelectorAll('.sidebar-menu');
-  for(let sidebar of sidebars){
-    sidebar.addEventListener('click', function(event){
+  for (let sidebar of sidebars) {
+    sidebar.addEventListener('click', function (event) {
       event.preventDefault();
       const idSidebar = sidebar.getAttribute('id');
       const pages = document.querySelectorAll('.container');
       sidebar.classList.remove('side-click');
-      for(let page of pages){
+      for (let page of pages) {
         page.classList.remove('active');
         const idPage = page.getAttribute('id');
-        if(idSidebar == idPage){
+        if (idSidebar == idPage) {
           page.classList.add('active');
           sidebar.classList.add('side-click');
           console.log('I add:', idSidebar);
@@ -74,32 +74,31 @@ sidebarActive();
 const ctx = document.getElementById('myChart').getContext('2d');
 // eslint-disable-next-line no-undef
 const chart = new Chart(ctx, {
-  // 1
+
   type: 'bar',
   data: {
-    // 2
+
     labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
-    // 3
+
     datasets: [{
-      // 4
+
       label: 'Signups',
-      // 5
       backgroundColor: '#8DBEC8',
       borderColor: '#8DBEC8',
-      // 6
-      data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
+
+      data: [52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
     },
     {
       label: 'FTD',
       backgroundColor: '#F29E4E',
       borderColor: '#F29E4E',
-      data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+      data: [6, 72, 1, 0, 47, 11, 50, 44, 63, 76],
     },
     {
       label: 'Earned',
       backgroundColor: '#71B374',
       borderColor: '#71B374',
-      data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+      data: [59, 49, 68, 90, 67, 41, 13, 38, 48, 48],
       // 7
       hidden: true,
     }]
